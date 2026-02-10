fc02d60e7141477cd2f85bd716e5fe389a734367
Yaren Kuruy <76249838+MrsBlueSky01@users.noreply.github.com>
2026-02-10 13:18:03 +0300

    ilk temiz yukleme: sadece java dosyalari

---
+++ src/main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args){
+        int a=4;
+        System.out.println(a);
+        Scanner scanner=new Scanner(System.in);
+    }
+}
